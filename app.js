791beb11ba32c6855a5decae26dddc476c9de601
romncn <rom.ncn@gmail.com>
2021-01-13 00:59:42 +0700

    make it better

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
 const express = require('express')
+const router = express.Router();
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
+
+router.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
+    if(msg === "rom"){
+       push(req)
+    } else {
+        reply(reply_token, msg)
+    }
     res.sendStatus(200)
 })
-app.listen(port)
+router.listen(port);
+
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {2bBnSBs3jWWl6deWeeGRlY/hwrKmid+DCmyVQZFvPzF8SnK+cTl8ICFTfwid5zUeSv55oLr+6HUIc6VzcWD3SKY8MCOYqSXWX8nZmUPa9PHrmG7xatUxlTWfn+mAK6rMMTmz/PY9JMY4ANUIeZkiIAdB04t89/1O/w1cDnyilFU=}'
     }
-
-    // switch (msg) {
-    //     case 'no':
-    //         let body = JSON.stringify({
-    //             replyToken: reply_token,
-    //             messages: [
-    //                 {
-    //                     type: 'text',
-    //                     text: "no"
-    //                 }
-    //             ]
-    //         })
-    //         return body;
-    //     case 'yes':
-    //         let body = JSON.stringify({
-    //             replyToken: reply_token,
-    //             messages: [
-    //                 {
-    //                     type: 'text',
-    //                     text: "yes"
-    //                 }
-    //             ]
-    //         })
-    //         return body;
-    //     default:
-    // }
 
     let body = JSON.stringify({
         replyToken: reply_token,
@@ -113,3 +95,27 @@ function reply(reply_token, msg) {
         console.log('status = ' + res.statusCode);
     });
 }
+function push(req) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {2bBnSBs3jWWl6deWeeGRlY/hwrKmid+DCmyVQZFvPzF8SnK+cTl8ICFTfwid5zUeSv55oLr+6HUIc6VzcWD3SKY8MCOYqSXWX8nZmUPa9PHrmG7xatUxlTWfn+mAK6rMMTmz/PY9JMY4ANUIeZkiIAdB04t89/1O/w1cDnyilFU=}'
+    }
+
+    let body = JSON.stringify({
+        to: "Uda66a8e7400b2e7fafd699f3b294ec4d",
+        messages: [
+            {
+                type: "text",
+                text: JSON.stringify(req.body)
+            }
+        ]
+    })
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+} 
